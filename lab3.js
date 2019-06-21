366a395a97c40be2c87392cd36a9fa79fae85476
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-06-21 00:48:22 -0500

    Update lab3.js

--- lab3.js
+++ lab3.js
@@ -1,20 +1,20 @@
+// Create a addbutton variable node retrieve from HTML button element.
 var addbutton = document.getElementById("button1");
+// Create a userInput variable node retrieve from HTML input element.
 var userInput = document.getElementById("inputToDoItem");
+// Create a user variable node retrieve from HTML ul element.
 var user = document.getElementById("userList");
 
+// Create a textAreaAddButton variable node retrieve from HTML button element.
 var textAreaAddButton = document.getElementById("button3");
+// Create a textAreaClearButton variable node retrieve from HTML button element.
 var textAreaClearButton = document.getElementById("button4");
 
 addbutton.addEventListener("click", buttonFunction1);
 clearbutton.addEventListener("click", buttonFunction2);
 
 
-
-
-
-
-function buttonFunction1() { 
-  
+function buttonFunction1() {   
     
     var placeholder = userInput.value;
     if (placeholder.trim()) {
@@ -31,40 +31,30 @@ function buttonFunction1() {
         addButton1.className = "remove";
         addButton1.addEventListener("click", removeItem);
         node.appendChild(addButton1);
-        user.appendChild(node);    
-        
-       
+        user.appendChild(node); 
          
     }        
 
     else {
         alert("You have not enter a 'Do-To' item! Please try again.");
-    }
-
-   
-    
+    }    
 }
 
 // clear entire list
 clearButton.addEventListener("click", buttonFunction2);
 function buttonFunction2 () {
-    user.innerHTML = " ";
-   
+    user.innerHTML = " ";   
 }
-
 
 textAreaAddButton.addEventListener("click", buttonFunction3);
 function buttonFunction3 () {
     var x = document.getElementById("message1").value;
-    document.getElementById("p2").innerHTML = x;   
-  
+    document.getElementById("p2").innerHTML = x;     
 }
 
 textAreaClearButton.addEventListener("click", buttonFunction4);
 function buttonFunction4 () {
-    p2.innerHTML = " ";
-    
-   
+    p2.innerHTML = " ";   
 }
 
 // remove a task from the list
