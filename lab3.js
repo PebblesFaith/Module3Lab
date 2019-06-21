e9c78b8fea828b6ce616c6a3adafaa16b843b7a7
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-06-20 23:22:43 -0500

    Update lab3.js

--- lab3.js
+++ lab3.js
@@ -57,12 +57,13 @@ textAreaAddButton.addEventListener("click", buttonFunction3);
 function buttonFunction3 () {
     var x = document.getElementById("message1").value;
     document.getElementById("p2").innerHTML = x;   
-    x.value = " ";
+  
 }
 
 textAreaClearButton.addEventListener("click", buttonFunction4);
 function buttonFunction4 () {
     p2.innerHTML = " ";
+    
    
 }
 
